e9af69be7bab8e612978e44bb5613611f28fe642
shishirkallapur <49324815+shishirkallapur@users.noreply.github.com>
2020-08-27 15:41:43 +0530

    Project

---
+++ CollegeAttendance-App-master/app/src/main/java/com/example/authenticate/FirebaseRecyclerViewOnClickListener.java
@@ -0,0 +1,8 @@
+package com.example.authenticate;
+import android.view.View;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+public interface FirebaseRecyclerViewOnClickListener
+{
+    void onItemClick(View view,DocumentSnapshot documentSnapshot, int position);
+}
---
+++ CollegeAttendance-App-master/app/src/main/java/com/example/authenticate/OnViewClickListener.java
@@ -0,0 +1,5 @@
+package com.example.authenticate;
+import android.view.View;
+public interface OnViewClickListener {
+    void onClick(View view, int position);
+}
